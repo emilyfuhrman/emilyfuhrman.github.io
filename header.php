ecf15863803d6712595d2545c21a4e8f02e8edf0
emilyfuhrman <emily.c.fuhrman@gmail.com>
2014-11-21 18:09:24 -0500

    header

---
+++ header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>EMILY FUHRMAN<?php echo $pageTitle ?></title>
+<meta name="description" content="Emily Fuhrman.">
+<meta name="keywords" content="emily fuhrman, emily, fuhrman, ylime, yxlime, y-lime, data visualization, data art, data artist, information design, Processing, code, graphic design, interactivity, William and Mary, William and Mary, Brooklyn, New York, NYC" />
+		<link rel='icon' href='favicon.ico' />
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<link rel="stylesheet" type="text/css" href="js/tipsy/tipsy.css">
+		
+		<script type="text/javascript" src="js/libs/jquery-1.10.2.min.js"></script>
+		<script src="js/libs/d3.v2.min.js"></script>
+		<script src="js/tipsy/jquery.tipsy.js"></script>
+		
+		<!-- google analytics -->
+		<script type="text/javascript">
+
+  		var _gaq = _gaq || [];
+  		_gaq.push(['_setAccount', 'UA-34550328-1']);
+  		_gaq.push(['_trackPageview']);
+
+  		(function() {
+    		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  		})();
+		</script>
+</head>
+<body>
+<link rel="icon" type="image/png" href="images/favicon.png" />
+	<header>
+		<nav class="links">
+			<a class='btn' href='http://y-li.me/'><div class="nav name">EMILY FUHRMAN</div></a>
+			<a class='btn' href='http://y-li.me/list/'><div class="nav proj">PROJECTS</div></a>
+			<a class='btn' href='http://y-li.me/emily-fuhrman/'><div class="nav abt">ABOUT</div></a>
+		</nav>
+	</header>
+	<div id="wrapper">
+		
