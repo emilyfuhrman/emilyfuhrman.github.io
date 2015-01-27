84171cac082916d5fd26e7e516ac8f639b7ee987
emilyfuhrman <emily.c.fuhrman@gmail.com>
2015-01-27 18:35:14 -0500

    ugh

--- js/vis_index.js
+++ js/vis_index.js
@@ -61,7 +61,7 @@ var init = function(){
 			var force = d3.layout.force()
 				.on("tick", tick)
 				.charge(-300)
-				.linkDistance(160)
+				.linkDistance(270)
 				.friction(0.8)
 				.size([w, h]);
 
