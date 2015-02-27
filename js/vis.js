3971905fddf196111269444d16d72d6acbca68cc
emilyfuhrman <emily.c.fuhrman@gmail.com>
2015-02-27 13:40:56 -0500

    setting start points

--- js/vis.js
+++ js/vis.js
@@ -1,12 +1,12 @@
 var schema = function(){
 
 	return {
-		transitionTime:200,
+		transitionTime:215,
 		vertices:{
-			1:{x:0,y:0},
-			2:{x:0,y:0},
-			3:{x:0,y:0},
-			4:{x:0,y:0}
+			1:{vx:0,vy:0,x:0,y:0},
+			2:{vx:0,vy:0,x:0,y:0},
+			3:{vx:0,vy:0,x:0,y:0},
+			4:{vx:0,vy:0,x:0,y:0}
 		},
 		generate:function(){
 			var self = vis,
@@ -15,7 +15,20 @@ var schema = function(){
 				oX = w/2,
 				oY = h/2;
 
-			function setPointPositions(){
+			function setPointStart(){
+				var dX = 350,
+					dY = 150;
+				self.vertices[1].vx = oX -dX;
+				self.vertices[1].vy = oY -dY;
+				self.vertices[2].vx = oX -dX;
+				self.vertices[2].vy = oY +dY;
+				self.vertices[3].vx = oX +dX;
+				self.vertices[3].vy = oY -dY;
+				self.vertices[4].vx = oX +dX;
+				self.vertices[4].vy = oY +dY;
+			}
+
+			function setPointEnd(){
 				self.vertices[1].x = oX -100;
 				self.vertices[1].y = oY -100;
 				self.vertices[2].x = oX -500;
@@ -37,15 +50,16 @@ var schema = function(){
 				vertL,
 				vertR;
 
-			setPointPositions();
+			setPointStart();
+			setPointEnd();
 
 			verticesG = svg.selectAll('g.vertex')
 				.data(d3.entries(self.vertices));
 			verticesG.enter().append('g')
 				.classed('vertex',true)
 				.attr('transform',function(d,i){
-					var x = w/2,
-						y = h/2;
+					var x = d.value.vx,//w/2,
+						y = d.value.vy;//h/2;
 					return 'translate(' + x + ',' + y + ')';
 				});
 			verticesG
@@ -53,20 +67,22 @@ var schema = function(){
 				.delay(500)
 				.ease('backs')
 				.duration(function(d,i){
-					var baseline = 100,
+					/*var baseline = 100,
 						s = self.transitionTime,
 						x = Math.abs(oX -d.value.x),
 						y = Math.abs(oY -d.value.y),
 
 						//triangulate distance to travel (from origin)
 						//scale to baseline distance
-						d = Math.sqrt(Math.pow(x,2) +Math.pow(y,2))/100;
-					return s;//d*s;
+						d = Math.sqrt(Math.pow(x,2) +Math.pow(y,2))/100;*/
+					return self.transitionTime;//d*s;
 				})
 				.attrTween('transform',function(d,i){
-					var x = d.value.x,
+					var vx = d.value.vx,
+						vy = d.value.vy,
+						x = d.value.x,
 						y = d.value.y,
-						string1 = 'translate(' + oX + ',' + oY + ')',
+						string1 = 'translate(' + vx + ',' + vy + ')',
 						string2 = 'translate(' + x + ',' + y + ')';
 					return d3.interpolateString(string1,string2);
 				})
