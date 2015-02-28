b1daa0d383b55f0c67f5eb21ebe9281478127dc8
emilyfuhrman <emily.c.fuhrman@gmail.com>
2015-02-27 22:40:19 -0500

    transition improvement

--- js/vis.js
+++ js/vis.js
@@ -3,46 +3,67 @@ var schema = function(){
 	return {
 		posts:JEKYLL_POSTS,
 		golden:(1+Math.sqrt(5))/2,
-		transitionTime:200,
-		vertices:{
-			1:{vx:0,vy:0,x:0,y:0},
-			2:{vx:0,vy:0,x:0,y:0},
-			3:{vx:0,vy:0,x:0,y:0},
-			4:{vx:0,vy:0,x:0,y:0}
+		transitionTime:220,
+		positions:{
+			1:{ 0:{x:0,y:0},
+				1:{x:0,y:0},
+				2:{x:0,y:0} },
+			2:{ 0:{x:0,y:0},
+				1:{x:0,y:0},
+				2:{x:0,y:0} },
+			3:{ 0:{x:0,y:0},
+				1:{x:0,y:0},
+				2:{x:0,y:0} },
+			4:{ 0:{x:0,y:0},
+				1:{x:0,y:0},
+				2:{x:0,y:0} }
 		},
 		generate:function(){
 			var self = vis,
 				w = window.innerWidth,
-				h = window.innerHeight
-				oX = w/2,
-				oY = h/2;
+				h = window.innerHeight,
 
-			function setPointStart(){
-				var dX = (w*1.25)/self.golden,
+				//half the width/height of one vertex	
+				sq = 36;
+
+			function setPointPos(){
+				var oX = w/2,
+					oY = h/2,
+					dX = (w*1.25)/self.golden,
 					dY = (h*1.25)/self.golden;
-				self.vertices[1].vx = w -dX;
-				self.vertices[1].vy = h -dY;
-				self.vertices[2].vx = w -dX;
-				self.vertices[2].vy = dY;
-				self.vertices[3].vx = dX;
-				self.vertices[3].vy = h -dY;
-				self.vertices[4].vx = dX;
-				self.vertices[4].vy = dY;
+
+				//state 0
+				self.positions[1][0].x = w -dX;
+				self.positions[1][0].y = 0 -(sq*2);
+				self.positions[2][0].x = w -dX;
+				self.positions[2][0].y = h +(sq*2);
+				self.positions[3][0].x = dX;
+				self.positions[3][0].y = 0 -(sq*2);
+				self.positions[4][0].x = dX;
+				self.positions[4][0].y = h +(sq*2);
+
+				//state 1
+				self.positions[1][1].x = w -dX;
+				self.positions[1][1].y = h -dY;
+				self.positions[2][1].x = w -dX;
+				self.positions[2][1].y = dY;
+				self.positions[3][1].x = dX;
+				self.positions[3][1].y = h -dY;
+				self.positions[4][1].x = dX;
+				self.positions[4][1].y = dY;
+
+				//state2
+				self.positions[1][2].x = oX -100;
+				self.positions[1][2].y = oY +200;
+				self.positions[2][2].x = oX -500;
+				self.positions[2][2].y = oY +100;
+				self.positions[3][2].x = oX +300;
+				self.positions[3][2].y = oY -100;
+				self.positions[4][2].x = oX +100;
+				self.positions[4][2].y = oY +100;
 			}
 
-			function setPointEnd(){
-				
-				//number of posts?
-				self.vertices[1].x = oX -100;
-				self.vertices[1].y = oY +200;
-
-				self.vertices[2].x = oX -500;
-				self.vertices[2].y = oY +100;
-				self.vertices[3].x = oX +300;
-				self.vertices[3].y = oY -100;
-				self.vertices[4].x = oX +100;
-				self.vertices[4].y = oY +100;
-			}
+			setPointPos();
 
 			var svg = d3.select('#vis')
 				.selectAll('svg')
@@ -55,19 +76,13 @@ var schema = function(){
 				vertL,
 				vertR;
 
-			//half the width/height of one vertex
-			var sq = 36;
-
-			setPointStart();
-			setPointEnd();
-
 			verticesG = svg.selectAll('g.vertex')
-				.data(d3.entries(self.vertices));
+				.data(d3.entries(self.positions));
 			verticesG.enter().append('g')
 				.classed('vertex',true)
 				.attr('transform',function(d,i){
-					var x = d.value.vx,
-						y = d.value.vy;
+					var x = self.positions[d.key][0].x,
+						y = self.positions[d.key][0].y;
 					return 'translate(' + x + ',' + y + ')';
 				});
 			verticesG
@@ -76,12 +91,23 @@ var schema = function(){
 				.ease('backs')
 				.duration(self.transitionTime)
 				.attrTween('transform',function(d,i){
-					var vx = d.value.vx,
-						vy = d.value.vy,
-						x  = d.value.x,
-						y  = d.value.y,
-						string1 = 'translate(' + vx + ',' + vy + ')',
-						string2 = 'translate(' + x  + ',' + y  + ')';
+					var x1 = self.positions[d.key][0].x,
+						y1 = self.positions[d.key][0].y,
+						x2 = self.positions[d.key][1].x,
+						y2 = self.positions[d.key][1].y;
+					var string1 = 'translate(' + x1 + ',' + y1 + ')',
+						string2 = 'translate(' + x2 + ',' + y2 + ')';
+					return d3.interpolateString(string1,string2);
+				})
+				.transition()
+				.delay(1000)
+				.attrTween('transform',function(d,i){
+					var x1 = self.positions[d.key][1].x,
+						y1 = self.positions[d.key][1].y,
+						x2 = self.positions[d.key][2].x,
+						y2 = self.positions[d.key][2].y;
+					var string1 = 'translate(' + x1 + ',' + y1 + ')',
+						string2 = 'translate(' + x2 + ',' + y2 + ')';
 					return d3.interpolateString(string1,string2);
 				});
 			vertBack = verticesG.selectAll('rect')
@@ -93,7 +119,6 @@ var schema = function(){
 				.attr('width',sq*2)
 				.attr('height',sq*2)
 				;
-			vertBack.exit().remove();
 			vertL = verticesG.selectAll('path.L')
 				.data(function(d){ return [d]; });
 			vertL.enter().append('path')
@@ -112,9 +137,10 @@ var schema = function(){
 					var pathString = 'M -' +sq + ', ' +sq +' L ' +sq +', -' +sq;
 					return pathString;
 				});
-			vertR.exit().remove();
-			vertL.exit().remove();
 			verticesG.exit().remove();
+			vertBack.exit().remove();
+			vertL.exit().remove();
+			vertR.exit().remove();
 		}
 	}
 }
