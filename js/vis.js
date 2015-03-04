3f2af1a30d9fae0dd1e00c2a2427bb25f6f0dfd2
emilyfuhrman <emily.c.fuhrman@gmail.com>
2015-03-04 17:04:56 -0500

    artificially resetting limit

--- js/vis.js
+++ js/vis.js
@@ -88,7 +88,7 @@ var schema = function(){
 				//set all ceilings to total # posts
 				//artificially shorten this limit for now
 				d3.entries(self.vertices).forEach(function(_d,_i){
-					self.vertices[_d.key].limit = self.posts.length*0.7;
+					self.vertices[_d.key].limit = self.posts.length*0.4;
 				});
 
 				//get actual values, paired with descriptive labels
