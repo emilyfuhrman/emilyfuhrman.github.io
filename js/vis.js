2380df526255100b46dec8ad405aecbc0ffa2e26
emilyfuhrman <emily.c.fuhrman@gmail.com>
2015-03-04 21:37:44 -0500

    tweax again

--- js/vis.js
+++ js/vis.js
@@ -3,9 +3,9 @@ var schema = function(){
 	return {
 		posts:JEKYLL_POSTS,
 		golden:(1+Math.sqrt(5))/2,
-		transitionTime:480,
+		transitionTime:240,
 		delay:240,
-		pause:240,
+		pause:120,
 		vertices:{
 			v1:{},
 			v2:{},
