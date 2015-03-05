99a3e41172868fe798d4d3ff9976bcd3ee8d2a5c
emilyfuhrman <emily.c.fuhrman@gmail.com>
2015-03-05 12:43:02 -0500

    is this it

--- js/vis.js
+++ js/vis.js
@@ -234,8 +234,8 @@ var schema = function(){
 
 			function transition() {
 				var t0_dur = self.transitionTime*2,
-					t1_dur = t0_dur*0.8,
-					t2_dur = t1_dur*0.8,
+					t1_dur = t0_dur,
+					t2_dur = t1_dur*0.6,
 
 					t0_del = (self.delay*2), //estimate
 					t1_del = t0_del +t0_dur +self.pause,
