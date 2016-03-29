b0f13967f7c32aca9025b5c0af4c2a3223b658d6
emilyfuhrman <emily.c.fuhrman@gmail.com>
2016-03-29 18:05:49 -0400

    adding extra option to index detection

--- js/master.js
+++ js/master.js
@@ -1,12 +1,12 @@
 window.onload = function(){
 	setFluid();
-	if(window.location.pathname === '/'){
+	if(window.location.pathname.length === 0 || window.location.pathname === '/'){
 		schema().generate();
 	}
 };
 window.onresize = function(){
 	setFluid();
-	if(window.location.pathname === '/'){
+	if(window.location.pathname.length === 0 || window.location.pathname === '/'){
 		schema().generate();
 	}
 };
