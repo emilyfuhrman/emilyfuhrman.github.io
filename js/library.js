9c530af327605becc5148a7ac10294ce8f00b48e
Emily Fuhrman <emilyfuhrman@Emilys-MacBook-Pro.local>
2019-05-09 16:57:53 -0400

    new

--- js/library.js
+++ js/library.js
@@ -2,7 +2,7 @@ var lib = function(){
 
 	return {
 
-		focus:"read",
+		focus:"authors",
 		order:"desc",
 
 		palette:['#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9','#bc80bd','#ccebc5','#ffed6f'],
@@ -33,6 +33,9 @@ var lib = function(){
 		data_tags:{
 			"data visualization": [
 					"graphical perception"
+				],
+			"math": [
+					"statistics"
 				],
 			"philosophy": [
 					"aesthetics"
