2d073dd015ce744c5929025483e813cd08b8cbcb
Emily Fuhrman <emilyfuhrman@Emilys-MacBook-Pro.local>
2019-05-17 17:13:39 -0400

    update

--- js/library.js
+++ js/library.js
@@ -5,7 +5,7 @@ var lib = function(){
 		focus:"read",
 		order:"desc",
 
-		palette:['#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9','#bc80bd','#ccebc5','#ffed6f'],
+		palette:['#8dd3c7','#fb8072','#bebada','#ffffb3','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9','#bc80bd','#ccebc5','#ffed6f'],
 
 		data_list:[],
 		data_columns:[
@@ -58,7 +58,7 @@ var lib = function(){
 					"pie charts",
 					"treemaps"
 				],
-			"form": [
+			"forms": [
 					"isomorphism"
 				],
 			"mapping":[
