2171f11745c494268985391517a3529afbd41340
emilyfuhrman <emily.c.fuhrman@gmail.com>
2019-05-27 09:21:30 -0400

    doc

--- js/library.js
+++ js/library.js
@@ -5,7 +5,7 @@ var lib = function(){
 		focus:"read",
 		order:"desc",
 
-		palette:['#8dd3c7','#fb8072','#bebada','#80b1d3','#ffffb3','#fdb462','#b3de69','#fccde5','#d9d9d9','#bc80bd','#ccebc5','#ffed6f'],
+		palette:['#8dd3c7','#fb8072','#bebada','#80b1d3','#fdb462','#ffffb3','#b3de69','#fccde5','#d9d9d9','#bc80bd','#ccebc5','#ffed6f'],
 
 		data_list:[],
 		data_columns:[
@@ -40,7 +40,11 @@ var lib = function(){
 		],
 		data_tags:{
 			"architecture":[
+				"urban planning",
 				"visionary architecture"
+			],
+			"art":[
+				"avant-garde"
 			],
 			"birds": [
 				"behavior"
