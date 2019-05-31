a3f82466eb40939f506569e01d7bad0a9a7797a6
Emily Fuhrman <emilyfuhrman@Emilys-MacBook-Pro.local>
2019-05-31 16:37:18 -0400

    order

--- js/library.js
+++ js/library.js
@@ -2,7 +2,7 @@ var lib = function(){
 
 	return {
 
-		focus:"read",
+		focus:"cats",
 		order:"desc",
 
 		palette:['#8dd3c7','#fb8072','#bebada','#80b1d3','#fdb462','#ffffb3','#b3de69','#fccde5','#d9d9d9','#bc80bd','#ccebc5','#ffed6f'],
