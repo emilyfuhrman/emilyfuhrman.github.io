6e97feb54ce3558ab8f297cdeb97bd2a1909d892
Emily Fuhrman <emilyfuhrman@Emilys-MacBook-Pro.local>
2019-06-07 17:09:56 -0400

    cats

--- js/library.js
+++ js/library.js
@@ -5,7 +5,7 @@ var lib = function(){
 		focus:"cats",
 		order:"asc",
 
-		palette:['#8dd3c7','#fb8072','#bebada','#80b1d3','#fdb462','#ffffb3','#b3de69','#fccde5','#d9d9d9','#bc80bd','#ccebc5','#ffed6f'],
+		palette:['#8dd3c7','#fb8072','#bebada','#80b1d3','#ffffb3','#fdb462','#b3de69','#fccde5','#d9d9d9','#bc80bd','#ccebc5','#ffed6f'],
 
 		data_list:[],
 		data_columns:[
@@ -57,10 +57,8 @@ var lib = function(){
 				"bird behavior"
 			],
 			"computer science": [
-					"algorithms"
-				],
-			"cybernetics": [
-					"information theory"
+					"algorithms",
+					"cybernetics"
 				],
 			"data visualization": [
 					"bar charts",
