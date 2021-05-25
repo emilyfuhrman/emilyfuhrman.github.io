6f43bf33c2784b5e50ff6862cfa91dfbf7f6ed60
emilyfuhrman <emily.c.fuhrman@gmail.com>
2021-05-24 23:58:37 -0400

    list

--- js/master.js
+++ js/master.js
@@ -8,3 +8,9 @@ $(".lang-switcher").click(function(e){
 	$("div.descrip:not([lang=' +default_lang +'])").hide();
 	$("div.descrip[lang=" +default_lang +"]").show();
 });
+
+//hide next/prev. arrows when navigating from list
+const URL_params = new URLSearchParams(window.location.search);
+if(URL_params.get('source') === 'list'){
+	$("#post-nav").hide();
+}
