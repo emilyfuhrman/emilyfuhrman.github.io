193fa2d6ad8b51aa09b22ade008300070b3a64df
Emily Fuhrman <emilyfuhrman@Emilys-MacBook-Pro.local>
2025-08-01 18:07:01 +0100

    image loading

--- js/loading.js
+++ js/loading.js
@@ -1,7 +1,9 @@
 //fade in once all images are done
 var $content = $('.container#main');
 
-$content.imagesLoaded().always(function(){
+$content.imagesLoaded({
+	background: false
+}).always(function(){
 	$('#loading-animation').addClass('hide');
 	$content.addClass('show');
 });
